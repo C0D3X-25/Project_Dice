bb61c1b0c64e0a4daa3e3b5cdfebc31ab988a9f9
C0D3X-25 <157924399+C0D3X-25@users.noreply.github.com>
2025-07-03 15:47:07 +0200

    Add: - Raylib to open a Window

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@
  *********************************************************************/
 #pragma once
 
+#include "raylib.h"
+
 #include "../include/CapacityPlayerSingleAttack.hpp"
 #include "../include/CapacityFactory.hpp"
 #include "../include/DiceCapacity.hpp"
@@ -19,57 +21,67 @@
 #include <memory>
 
 
+
+using namespace dice;
+using namespace entity;
+using namespace capacity;
+using namespace group;
+
 int main() {
 
-	using namespace dice;
-	using namespace entity;
-	using namespace capacity;
-	using namespace group;
-	
-	CapacityFactory capacity_factory;
-	
-	PlayerEntityGenerator entity_generator;
-	BasePlayerEntity entity_1 = entity_generator.generateNewPlayerEntity();
-	//BasePlayerEntity entity_2 = entity_generator.generateNewPlayerEntity();
-	//BasePlayerEntity entity_3 = entity_generator.generateNewPlayerEntity();
-	//BasePlayerEntity entity_4 = entity_generator.generateNewPlayerEntity();
-	//BasePlayerEntity entity_5 = entity_generator.generateNewPlayerEntity();
-	//BasePlayerEntity entity_6 = entity_generator.generateNewPlayerEntity();
-	//entity_6.setCapacity(capacity_factory.attackRandom(entity_6), 3);
+	 SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+	 InitWindow(800, 450, "Project Dice");
 
-	//Team team_1("Team 1");
-	//team_1.addEntity(std::make_unique<BasePlayerEntity>(entity_1), 0);
-	//team_1.addEntity(std::make_unique<BasePlayerEntity>(entity_2), 0);
-	//team_1.addEntity(std::make_unique<BasePlayerEntity>(entity_3), 0);
-	//team_1.addEntity(std::make_unique<BasePlayerEntity>(entity_4), 0);
+	 CapacityFactory capacity_factory;
 
-	//Team team_2("Team 2");
-	//team_2.addEntity(std::make_unique<BasePlayerEntity>(entity_5), 0);
-	//team_1.transferEntityTo(team_2, 1, 0);
-	//team_2.addEntity(std::make_unique<BasePlayerEntity>(entity_6), 0);
+	 PlayerEntityGenerator entity_generator;
+	 BasePlayerEntity entity_1 = entity_generator.generateNewPlayerEntity();
+	 //BasePlayerEntity entity_2 = entity_generator.generateNewPlayerEntity();
+	 //BasePlayerEntity entity_3 = entity_generator.generateNewPlayerEntity();
+	 //BasePlayerEntity entity_4 = entity_generator.generateNewPlayerEntity();
+	 //BasePlayerEntity entity_5 = entity_generator.generateNewPlayerEntity();
+	 //BasePlayerEntity entity_6 = entity_generator.generateNewPlayerEntity();
+	 //entity_6.setCapacity(capacity_factory.attackRandom(entity_6), 3);
 
-	//team_1.printGroup();
-	//team_2.printGroup();
+	 //Team team_1("Team 1");
+	 //team_1.addEntity(std::make_unique<BasePlayerEntity>(entity_1), 0);
+	 //team_1.addEntity(std::make_unique<BasePlayerEntity>(entity_2), 0);
+	 //team_1.addEntity(std::make_unique<BasePlayerEntity>(entity_3), 0);
+	 //team_1.addEntity(std::make_unique<BasePlayerEntity>(entity_4), 0);
 
-	std::cout << "========================================================\n\n";
-	BaseCapacity capacity = capacity_factory.attackSingle(entity_1);
-	std::cout << "Before: " << entity_1.getCurrentLife() << '\n';
-	CapacityComponent capacity_modifier = capacity.getNextCapacityModifier();
-	entity_1.resolveCapacity(capacity_modifier, entity_1);
-	std::cout << "After:  " << entity_1.getCurrentLife() << '\n';
+	 //Team team_2("Team 2");
+	 //team_2.addEntity(std::make_unique<BasePlayerEntity>(entity_5), 0);
+	 //team_1.transferEntityTo(team_2, 1, 0);
+	 //team_2.addEntity(std::make_unique<BasePlayerEntity>(entity_6), 0);
 
+	 //team_1.printGroup();
+	 //team_2.printGroup();
 
-	//entity_1.printEntity();
-	//std::cout << "========================================================\n\n";
-	//entity_2.printEntity();
-	//std::cout << "========================================================\n\n";
+	 std::cout << "========================================================\n\n";
+	 BaseCapacity capacity = capacity_factory.attackSingle(entity_1);
+	 std::cout << "Before: " << entity_1.getCurrentLife() << '\n';
+	 CapacityComponent capacity_modifier = capacity.getNextCapacityModifier();
+	 entity_1.resolveCapacity(capacity_modifier, entity_1);
+	 std::cout << "After:  " << entity_1.getCurrentLife() << '\n';
 
+	 // Game loop
+	 while (!WindowShouldClose()) {
 
+		 BeginDrawing();
+		 ClearBackground(RAYWHITE);
 
+		 DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
 
-	//for (size_t i = 0; i < 20; i++) {
-	//	BaseCapacity cap = entity_1.rollDiceCapacity();
-	//	std::cout << "Roll No " << i + 1 << ": "  << cap.getCapacityName() << '\n';
-	//}
+		 EndDrawing();
+	 }
+
+	 CloseWindow();
 }
+
+
+
+
+	
+	
+
 
